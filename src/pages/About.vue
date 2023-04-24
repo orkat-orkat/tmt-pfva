be460c4e55432daf89a8968a169dc7903aea61f6
tomic <ocean.blue.sky@gmail.com>
2023-04-25 00:09:30 +0900

    整理

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -8,10 +8,9 @@ import Projects from '@/pages/Projects.vue'
   <h4 class="subtitle1">About</h4>
   <div class="self-wrap">
   <ul class="about-txt">
-            <li>これまで行ってきた仕事や、仕事内容について忘備録を兼ねてリストアップしています。</li>
-            <li>主にB to B に関する幅広いWeb制作、アプリ制作の制作経験があります。MSOffice, Adobe製品を使うことができます。
-            <br>プロトタイプ作成には主にXDを使い、プロジェクトによりMiroなどを使いデータを収集することもあります。
-</li>
+        <li>これまで行ってきた仕事や、仕事内容について忘備録を兼ねてリストアップしています。</li>
+        <li>主にB to B に関する幅広いWeb制作、アプリ制作の制作経験があります。MSOffice, Adobe製品を使うことができます。
+        <br>プロトタイプ作成には主にXDを使い、プロジェクトによりMiroなどを使いデータを収集することもあります。</li>
         </ul>
     </div>
 
@@ -23,24 +22,31 @@ import Projects from '@/pages/Projects.vue'
         <li class="current">UX Designer</li>
         <li>NEXT STAGE</li>
     </ol>
-    
-  
+
+
     <div class="self-wrap">
-      <div class="my-history">
-      <div class="self-history-items">User Experience（体験構築）</div>
+        <div class="self-items5">
+        User Research（ユーザー理解）
+      </div>
+      
       <div class="self-history"><font-awesome-icon :icon="['fas', 'plus']" /></div>
-      <div class="self-history-items">User Research（ユーザー理解）</div>
+      <div class="self-items5">
+        User Experience（体験構築）
+      </div>
       <div class="self-history"><font-awesome-icon :icon="['fas', 'plus']" /></div>
-      <div class="self-history-items">Technology（技術）</div>
-    </div>
-    </div>
+      <div class="self-items5">
+        要件定義(Business Requirement)
+      </div>
+      <div class="self-history"><font-awesome-icon :icon="['fas', 'plus']" /></div>
+      <div class="self-items5">
+        Technology（技術）
+      </div>
+  </div>
 
-
-
-    <p class="about-txt">上記三本柱で、仕事をしてきました。<br>将来はプロダクトやサービス全体の専門家として、もっと経験を積みたいと思っています。<br>
+    <p class="about-txt">上記を軸にして仕事をしています。<br>将来はプロダクトやサービス全体の専門家として、もっと経験を積みたいと思っています。<br>
         興味があるのは、DXや業務改善、労働改善からの → 経営改善アプローチです。<br>
-        チームメンバーとコミュニケーションをとりながらプロダクト改善、プロダクト開発をしていきたいです。<br>
-        理想の働き方はリモートワークで、海外からでも仕事をできるにようになりたいです。<br>
+        チームメンバーとコミュニケーションをとりながらプロダクト改善、プロダクト開発に貢献していきたいです。<br>
+        理想の働き方はリモートワークで、海外からでも仕事をできるにようになりたいです。
     </p>
  
   <!--- Project pageから-->
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -5,24 +5,24 @@
   <div class="self-wrap">
       <div class="about-box">
         <h5 class="about-sub-title">Project List</h5>
-        <ul class="about-box-list project-ov">
-            <li>[Sony]次世代車載製品のUX体験開発に参画<br>Participated in new product development</li>
+        <ul class="about-box-list project-li">
+            <li>[Sony]次世代車載製品と、音楽アプリのUX体験開発に参画<br>Participated in new product development</li>
             <li>[富士ソフト]ベンチャー企業の金融サイト改善に関するユーザー調査</li>
             <li>[富士ソフト]ドローン操作用画面、車の配車アプリなどのUI開発</li>
         </ul>
       </div>
-      <div class="self-items3">
+      <div class="self-items3 self-item-sub1">
         <h5 class="about-sub-title">対象製品、サービス</h5>
-        <ul class="about-box-list">
+        <ul class="about-box-list project-prod">
             <li>次世代車載製品</li>
             <li>MDR(ヘッドホン、音楽アプリ、ハブアプリ)</li>
             <li>社内UX人材教育資料作成</li>
             <li>ドローン用UI開発</li>
         </ul>
       </div>
-      <div class="self-items3">
+      <div class="self-items3 self-item-sub2">
         <h5 class="about-sub-title">UX Researcher / UX Designer</h5>
-        <ul class="about-box-list">
+        <ul class="about-box-list project-skills">
             <li>ヒューリスティック調査</li>
             <li>デザインリサーチ</li>
             <li>競合リサーチ</li>
@@ -35,31 +35,33 @@
   <div class="self-wrap">
       <div class="about-box">
         <h5 class="about-sub-title">Project List</h5>
-        <ul class="about-box-list project-ov">
+        <ul class="about-box-list project-li">
             <li>マイクロソフトの製品、技術を使い、多くの企画に参画。</li>
             <li>大手石油会社内部システム刷新</li>
-            <li>[UD Truck]のCMS開発をユーザーリサーチ（インタビュー含む）などの上流工程から参画</li>
+            <li>[UD Truck]CMS開発をUX上流工程から参画</li>
             <li>[郵政] 大規模PCマイグレーションプロジェクトのテストフェーズ、サブリーダーとして参画</li>
-            <li>[KOMATSU] Azureを利用した次世代重機の車両開発PoCプロジェクトに参画し、画面や提案書を作成</li>
+            <li>[KOMATSU] Azureを利用した次世代重機の車両開発PoCプロジェクトに参画し、画面作成や提案書を作成</li>
             <li>[神明] 農業生産管理システム構築</li>
             <li>[UNIQRO] ユニクロ社内の人材開発教材アプリと、従業員用のアプリを作成</li>
+            <li>[小学館]教育のICTアプリ</li>
             <li>[社内] 社内プロジェクトでVR開発、外国人従業者の相談をサポート</li>
         </ul>
       </div>
-      <div class="self-items3">
+      <div class="self-items3 self-item-sub1">
         <h5 class="about-sub-title">経験業界、経験フェーズ</h5>
-        <ul class="about-box-list">
+        <ul class="about-box-list project-prod">
             <li>車載製品</li>
             <li>農業業界でのIoT利用のためのPoC</li>
             <li>大規模PCマイグレーション</li>
             <li>物流会社のシステム更改に関するテストフェーズ</li>
             <li>商社のイントラ設計</li>
+            <li>教育のICTアプリ</li>
         </ul>
       </div>
 
-      <div class="self-items3">
+      <div class="self-items3 self-item-sub2">
         <h5 class="about-sub-title">Experience Designer</h5>
-        <ul class="about-box-list">
+        <ul class="about-box-list project-skills">
             <li>O365移行</li>
             <li>画面設計</li>
             <li>PoC</li>
@@ -73,36 +75,56 @@
 
   <div class="self-wrap">
       <div class="about-box">
-        <h5 class="about-sub-title">In-House Designer</h5>
-        <ul class="about-box-list">
-            <li>Experience Designer</li>
+        <h5 class="about-sub-title">Project List</h5>
+        <ul class="about-box-list project-li">
             <li>[NHK]Make PR and advertise TV Programming on Internet </li>
-            <li>[NHK]Make PR and advertise TV Programming on Internet </li>
+            <li>[Marui] Create EC Webistes and Teaser Site</li>
+            <li>Create many Websites</li>
         </ul>
       </div>
       <div class="self-items3">
-        <h5 class="about-sub-title">Web Designer</h5>
-        <ul class="about-box-list">
-            <li>車載製品</li>
-            <li>MDR(ヘッドホン、音楽製品)</li>
-            <li>社内UX人材教育資料作成</li>
+        <h5 class="about-sub-title">In-House Web Designer Coding</h5>
+        <ul class="about-box-list project-skills">
+            <li>Web Design / Coding</li>
+            <li>SEO</li>
+            <li>PR</li>
+            <li>Video Editor</li>
         </ul>
       </div>
   </div>
 
-  <div class="output">
+<div class="output">
   <h4 class="subtitle1">Output / Deliverables</h4>
-  <div class="self-wrap">
-    <div class="output-items">User Research</div>
-    <div class="output-items">Interview</div>
-    <div class="output-items">Worekshop</div>
-    <div class="output-items">Customer J Map</div>
-    <div class="output-items">KJ Method</div>
-    <div class="output-items">Wireflame</div>
-    <div class="output-items">BRD</div>
-    <div class="output-items">PoC</div>
+<div class="self-wrap">
+  <div class="output-items">
+        User Research
+      </div>
+      <div class="output-items">
+        Interview
+      </div>
+      <div class="output-items">
+        Worekshop
+      </div>
+      <div class="output-items">
+        Customer J Map
+      </div>
+    </div>
+      <div class="self-wrap">
+        <div class="output-items">
+        KJ Method
+      </div>
+      <div class="output-items">
+        Wireflame
+      </div>
+      <div class="output-items">
+        BRD
+      </div>
+      <div class="output-items">
+        PoC
+      </div>
+    </div>
   </div>
-</div>
+
 
   <div class="programming">
   <h4 class="subtitle1">Programming</h4>
